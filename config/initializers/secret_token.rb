2cd93f97cf8db94c77cee7eebf220f79b5e0e8b4
SergeyBerdnikovich <atom@tut.by>
2013-04-14 13:09:25 +0300

    Initial Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Perepiska::Application.config.secret_token = '2f41f5d970f5bcc0c70653631d88afddd71bc352bceb1b5e74d74ecd2a410e703b1080e1e40dbbe48b6367de24cb65fe5985c972d09478d1865b82add8cda31f'
