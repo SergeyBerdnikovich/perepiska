547fab6ad9af1da869d388eaa46da73eb8605876
SergeyBerdnikovich <atom@tut.by>
2013-04-15 16:44:59 +0300

    updated reload divs - ajax - remote = true

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,16 +16,16 @@
 //= require_tree .
 
 $(document).ready(function(){
-  $('div.emails a').click(function(){
-    var id = $(this).attr("id");
-    $.get('main/get_messages', { email_id: id });
-  })
-  $(document).on("click","div.email_messages a",function(e){
-    var id = $(this).attr("id");
-    $.get('main/get_message_body', { message_id: id });
-  });
-  $('div.admin_panel a').click(function(){
-    var id = $(this).attr("id");
-    $.get('main/get_admin_panel', { admin_panel_id: id });
-  })
+  // $('div.emails a').click(function(){
+  //   var id = $(this).attr("id");
+  //   $.get('main/get_messages', { email_id: id });
+  // })
+  // $(document).on("click","div.email_messages a",function(e){
+  //   var id = $(this).attr("id");
+  //   $.get('main/get_message_body', { message_id: id });
+  // });
+  // $('div.admin_panel a').click(function(){
+  //   var id = $(this).attr("id");
+  //   $.get('main/get_admin_panel', { admin_panel_id: id });
+  // })
 });
